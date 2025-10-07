2eec743084184006d30a5e4591755f37f2c8d183
Farsana <farsanaserin97@gmail.com>
2025-10-07 22:37:28 +0530

    vercel url created

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(cors({origin:'https://travel-booking-app-swart.vercel.app'}))
+
 
 app.use("/api/auth", authRouter);
 app.use("/api/destination", destiRouter);
