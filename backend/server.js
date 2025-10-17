16cf2896026b7057e57554953f9b3a8843190cdc
Farsana <farsanaserin97@gmail.com>
2025-10-17 19:48:07 +0530

    reployed frontend

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({origin:'https://travel-booking-app-swart.vercel.app'}))
+app.use(cors({origin:'https://travel-booking-app-ruddy.vercel.app'}))
 
 // app.use(cors({origin:'http://localhost:5173'}))
 
